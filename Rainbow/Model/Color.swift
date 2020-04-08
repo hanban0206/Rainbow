17afa3e23626185c9048107162bd5d608b457f8e
Hannah <hannahlbain89@gmail.com>
2020-04-07 21:34:18 -0500

    set up color model

---
+++ Rainbow/Model/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Hannah Bain on 4/7/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    var colorName: String
+    var color: UIColor
+}
+
